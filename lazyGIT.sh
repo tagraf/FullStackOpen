77533cce53a535b556dfdfadbec49e40ec5d22e2
tagraf <tagraf@gmail.com>
2020-07-18 01:35:54 -0400

    testing one two

--- lazyGIT.sh
+++ lazyGIT.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Saves time to go through the routine
 
-if [ -z "$@" ]; then 
+if [ -z "$1" ]; then 
   echo ""
   echo "EXAMPLE COMMAND:   ./lazyGIT.sh 'comments for commit'"
   echo ""
 else
   git add -A
-  git commit -m "$@"
+  git commit -m "$1"
   git push origin FullStackOpen
   git checkout master
   git merge FullStackOpen
