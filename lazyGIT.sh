76f40a6e33425cb96996eadb308e718b8e0a7db3
tagraf <tagraf@gmail.com>
2020-07-18 01:16:11 -0400

    1

--- lazyGIT.sh
+++ lazyGIT.sh
@@ -1,8 +1,17 @@
-git add -A
-git commit -m "-"
-git push origin FullStackOpen
-git checkout master
-git merge FullStackOpen
-git push origin master
-git checkout FullStackOpen
+#!/bin/bash
+# Saves time to go through the routine
+
+if [ -z "$1" ]; then 
+  echo ""
+  echo "EXAMPLE COMMAND:   ./lazyGIT.sh 'comments for commit'"
+  echo ""
+else
+  git add -A
+  git commit -m "1"
+  git push origin FullStackOpen
+  git checkout master
+  git merge FullStackOpen
+  git push origin master
+  git checkout FullStackOpen
+fi
 
