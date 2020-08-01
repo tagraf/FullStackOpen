08ab3774b99c2c4e6dc4113fe460b8c8496e8763
tagraf <tagraf@gmail.com>
2020-08-01 19:50:10 -0400

    better version of a bug

--- p1/unicafe/src/index.js
+++ p1/unicafe/src/index.js
@@ -34,9 +34,9 @@ const UI = (props) => {
 
 const Stats = (props) => {
 console.log("Stats: ",props.objRate.value.g)
-//if ({props.objRate.value.g}) !== 0 {
+if ({props.objRate.value.g}) !== 0) {
 //if ( {props.objRate.value.g} !== 0 ) && {props.objRate.value.n} !== 0 && {props.objRate.value.b} !== 0 ){
-if (1 !== 1) {
+//if (1 !== 1) {
   return (
     <div>
     <h2>Stats:</h2>
